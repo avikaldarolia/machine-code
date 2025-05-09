cbba64eb7fe77a2c90c80452965dfca4facd24ef
Avikal Darolia <avikaldarolia7@gmail.com>
2025-05-09 13:49:13 -0400

    Optimised

--- src/app/problems/nested-checkbox/page.tsx
+++ src/app/problems/nested-checkbox/page.tsx
@@ -5,7 +5,6 @@ type Checkbox = {
 	id: number;
 	label: string;
 	checked: boolean;
-	parent: undefined | number;
 	children?: Checkbox[];
 };
 
@@ -42,98 +41,67 @@ const CheckboxesData: ogCheck[] = [
 	},
 ];
 
-const buildData = (boxes: ogCheck[], parent: number | undefined): Checkbox[] => {
-	return boxes.map((box) => {
-		if (box.children) {
-			box.children = buildData(box.children, box.id);
-		}
-
-		return {
-			...box,
-			parent,
-		} as Checkbox;
-	});
+const buildTree = (nodes: ogCheck[]): Checkbox[] => {
+	return nodes.map((node) => ({
+		id: node.id,
+		label: node.label,
+		checked: false,
+		children: node.children ? buildTree(node.children) : undefined,
+	}));
 };
 
 const NestedCheckbox = () => {
-	const [checkboxesData, setCheckboxesData] = useState<Checkbox[]>(buildData(CheckboxesData, undefined));
+	const [checkboxes, setCheckboxes] = useState<Checkbox[]>(buildTree(CheckboxesData));
 
-	const updateTree = (boxes: Checkbox[], cb: (box: Checkbox) => boolean): Checkbox[] => {
-		return boxes.map((box) => {
-			if (cb(box)) {
-				return box;
-			}
+	const updateCheckboxTree = (nodes: Checkbox[], targetId: number, isChecked: boolean): Checkbox[] => {
+		return nodes.map((node) => {
+			const updatedNode = { ...node };
 
-			if (box.children) {
-				box.children = updateTree(box.children, cb);
-			}
-
-			return box;
-		});
-	};
-
-	const ut = (boxes: Checkbox[], cb: (box: Checkbox) => number): Checkbox[] => {
-		return boxes.map((box) => {
-			if (box.children) {
-				box.children = ut(box.children, cb);
-				const count = cb(box);
-				if (count === box.children.length) {
-					box.checked = true;
-				} else {
-					box.checked = false;
+			if (node.id === targetId) {
+				updatedNode.checked = isChecked;
+				if (updatedNode.children) {
+					updatedNode.children = toggleChildren(updatedNode.children, isChecked);
 				}
+			} else if (node.children) {
+				updatedNode.children = updateCheckboxTree(node.children, targetId, isChecked);
 			}
 
-			return box;
+			if (updatedNode.children) {
+				const allChecked = updatedNode.children.every((child) => child.checked);
+				updatedNode.checked = allChecked;
+			}
+
+			return updatedNode;
 		});
 	};
 
-	const handleClick = (id: number) => {
-		let boxes = [...checkboxesData];
-		boxes = updateTree(boxes, (box) => {
-			if (box.id === id) {
-				box.checked = !box.checked;
-				if (box.children) {
-					updateTree(box.children, (b) => {
-						b.checked = box.checked;
-						return false;
-					});
-				}
-				return true;
-			}
-
-			return false;
-		});
-
-		boxes = ut(boxes, (box) => {
-			if (box.children && box.children.length > 0) {
-				let count = 0;
-				box.children.forEach((b) => {
-					count += b.checked ? 1 : -1;
-				});
-
-				return count;
-			}
-			return 0;
-		});
-
-		setCheckboxesData(boxes);
+	const toggleChildren = (nodes: Checkbox[], isChecked: boolean): Checkbox[] => {
+		return nodes.map((node) => ({
+			...node,
+			checked: isChecked,
+			children: node.children ? toggleChildren(node.children, isChecked) : undefined,
+		}));
 	};
 
-	const render = (data: Checkbox[]) => {
-		return data?.map((box: Checkbox) => (
-			<div key={box.id} className="w-full ml-3">
+	const handleCheckboxChange = (id: number, isChecked: boolean) => {
+		const updatedTree = updateCheckboxTree(checkboxes, id, isChecked);
+		setCheckboxes(updatedTree);
+	};
+
+	const renderCheckboxes = (nodes: Checkbox[]) => {
+		return nodes?.map((node: Checkbox) => (
+			<div key={node.id} className="w-full ml-3">
 				<input
 					type="checkbox"
-					name={box.label}
-					id={box.id.toString()}
-					checked={box.checked}
-					onChange={() => handleClick(box.id)}
+					name={node.label}
+					id={`checkbox-${node.id}`}
+					checked={node.checked}
+					onChange={(e) => handleCheckboxChange(node.id, e.target.checked)}
 				/>
-				<label htmlFor="" className="ml-3">
-					{box.label}
+				<label htmlFor={`checkbox-${node.id}`} className="ml-3">
+					{node.label}
 				</label>
-				{box.children && render(box.children)}
+				{node.children && renderCheckboxes(node.children)}
 			</div>
 		));
 	};
@@ -141,7 +109,7 @@ const NestedCheckbox = () => {
 	return (
 		<div className="p-6 w-1/3 mx-auto flex flex-col justify-center items-center">
 			<h1 className="text-4xl py-10">Nested Checkbox</h1>
-			{render(checkboxesData)}
+			{renderCheckboxes(checkboxes)}
 		</div>
 	);
 };
